91882ccc3bd41604422197ad8f3b4ab3394bffc9
Anjali Bhawari <100554023+Anjali1822@users.noreply.github.com>
2026-01-20 19:32:53 +0530

    Update 2_Object_Detection_-_Roboflow_Train.py

--- pages/2_Object_Detection_-_Roboflow_Train.py
+++ pages/2_Object_Detection_-_Roboflow_Train.py
@@ -334,4 +334,11 @@ if uploaded_file_od != None:
     uploaded_img = np.array(image)
     inferenced_img = uploaded_img.copy()
 
-    run_inference(workspace_id, model_id, version_number, uploaded_img, inferenced_img)
+ 
+run_inference(
+    st.session_state.workspace_id,
+    st.session_state.model_id,
+    st.session_state.version_number,
+    uploaded_img,
+    inferenced_img
+)
